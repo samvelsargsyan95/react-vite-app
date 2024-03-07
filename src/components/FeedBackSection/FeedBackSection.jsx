eff953a03225502cee89da29c7ac323d72a99c49
samvelsargsyan95 <samvelsargsyan0995@gmail.com>
2024-03-08 00:51:31 +0400

    Changed 'inputs' usable logic, with the form object

--- src/components/FeedBackSection/FeedBackSection.jsx
+++ src/components/FeedBackSection/FeedBackSection.jsx
@@ -3,56 +3,73 @@ import { useState } from 'react'
 import './FeedBackSection.scss'
 
 export default function FeedBackSection() {
-    const [name, setName] = useState('')
-    const [reason, setReason] = useState('help')
-    const [message, setMessage] = useState('')
-    const [nameError, setNameError] = useState(false)
-    const [messageError, setMessageError] = useState(false)
+    const [form, setForm] = useState({
+        name: '',
+        reason: 'help',
+        message: '',
+        nameError: false,
+        messageError: false
+    })
+    // const [name, setName] = useState('')
+    // const [reason, setReason] = useState('help')
+    // const [message, setMessage] = useState('')
+    // const [nameError, setNameError] = useState(false)
+    // const [messageError, setMessageError] = useState(false)
     const [successText, setSuccessText] = useState('')
     const [showForm, setShowForm] = useState(true)
 
     const handleNameChange = (event) => {
-        setName(event.target.value)
-        setNameError(!event.target.value.trim())
+        setForm({
+            name: event.target.value,
+            nameError: !event.target.value.trim()
+        })
     }
 
     const handleMessageChange = (event) => {
-        setMessage(event.target.value)
-        setMessageError(!event.target.value.trim())
+        setForm((prev) => ({
+            ...prev,
+            message: event.target.value,
+            messageError: !event.target.value.trim()
+        }))
     }
 
     const formSubmit = (event) => {
         event.preventDefault()
-        if (!name.trim()) {
-            setNameError(true)
+        if (!form.name.trim()) {
+            setForm((prev) => ({ ...prev, nameError: true }))
         }
 
-        if (!message.trim()) {
-            setMessageError(true)
+        if (!form.message.trim()) {
+            setForm((prev) => ({ ...prev,  messageError: true }))
         }
 
-        if (!name.trim() || !message.trim()) {
+        if (!form.name.trim() || !form.message.trim()) {
             return;
         }
 
-        if (name && message) {
+        if (form.name && form.message) {
             setSuccessText(`
-                <p>Dear ${ name } thanks for the feedback...</p>
-                <p><b>Reason:</b> ${ reason }</p>
-                <p><b>Message:</b> ${ message }</p>
+                <p>Dear ${ form.name } thanks for the feedback...</p>
+                <p><b>Reason:</b> ${ form.reason }</p>
+                <p><b>Message:</b> ${ form.message }</p>
             `)
+
+            setShowForm(false)
         }
 
-        setShowForm(false)
-        setName('');
-        setReason('help');
-        setMessage('');
+        setForm({
+            name: '',
+            reason: 'help',
+            message: '',
+            nameError: false,
+            messageError: false
+        })
     }
 
     return (
         <section>
-            <h3 className="mb-2">Feed Back {showForm}</h3>
-
+            <h3 className="mb-2">Feed Back</h3>
+ 
             {
                 showForm
                     ? 
@@ -60,9 +77,9 @@ export default function FeedBackSection() {
                             <div className="mb-2">
                                 <label htmlFor="name">Name</label> <br />
                                 <input
-                                    value={name}
+                                    value={form.name}
                                     onChange={handleNameChange}
-                                    style={{boxShadow: nameError ? 'inset 0 0 0 1px red' : null}}
+                                    style={{boxShadow: form.nameError ? 'inset 0 0 0 1px red' : null}}
                                     type="text" 
                                     id="name" 
                                     className="control" 
@@ -72,8 +89,8 @@ export default function FeedBackSection() {
                             <div className="mb-2">
                                 <label htmlFor="reason">Reason</label> <br />
                                 <select 
-                                    value={reason}
-                                    onChange={event => setReason(event.target.value)}
+                                    value={form.reason}
+                                    onChange={event => setForm((prev) => ({ ...prev, reason: event.target.value }))}
                                     id="reason" 
                                     className="control" 
                                 >
@@ -86,9 +103,9 @@ export default function FeedBackSection() {
                             <div className="mb-2">
                                 <label htmlFor="message">Message</label> <br />
                                 <textarea 
-                                    value={message}
+                                    value={form.message}
                                     onChange={handleMessageChange}
-                                    style={{boxShadow: messageError ? 'inset 0 0 0 1px red' : null}}
+                                    style={{boxShadow: form.messageError ? 'inset 0 0 0 1px red' : null}}
                                     id="message" 
                                     className="control"
                                 ></textarea>
@@ -96,7 +113,7 @@ export default function FeedBackSection() {
 
                             <div>
                                 <Button
-                                    disabled={nameError || messageError}
+                                    disabled={form.nameError || form.messageError}
                                     onClick={formSubmit}
                                 >Send</Button>
                             </div>
